c8813d4d130b0976f5ea7eb618820cb04b4a21c9
Donovan Coleman <student.150757@worcesterschools.net>
2019-05-17 14:01:14 +0000

    third commit

---
+++ labs/36D.py
@@ -0,0 +1,5 @@
+Don = input(">")
+print("1 or 2")
+input(">")
+if  Don == "one" :
+    print("1")
--- labs/studyDrillex36.py
+++ labs/studyDrillex36.py
@@ -1,21 +1,65 @@
 print("""You wake up and look around. 
-Your in a field with only lush green tree's darkened by the knight sky.""")
+Your in a field with only lush green tree's darkened by the night sky.""")
 print("?:Where am I? Oww! my head.")
 print("The moon becomes brighter and brighter. The light begins to bother you and you can't see")
-print("Lunar:You there bask in the glory of Lunar the goddes of night what are you")
-print("1.I'm a Boy 2. I'm a Girl 3. ...")
-input (">")
-if input == "a" :
+print(" Lunar: You there, bask in the glory of Lunar the goddes of night what are you")
+print("The 7 foot woman stares intensly at you awaiting your answer. She's wearing a black dress with a white moon cresent on her back however barefoot.")
+print("1.I'm a Boy \n 2. I'm a Girl \n 3. ...")
+Don = input(">")
+if Don == "1" :
     print("Lunar:Good for you, I meant your name what are you called.")
-elif input == "b" :
+if Don == "2" :
     print("Lunar:You are a very... dense I meant your name.")
-elif input == "c" :
-    print("Lunar:You... did hear me correct, hello. Crap another silent type I thought kids were getting smarter oh whatever i'll call you a dissapointment ")
-elif input != "1" or "2" or "3" :
+if Don == "3" :
+    print("Lunar:You... did hear me correct, hello. Crap another silent type I thought kids were getting smarter oh whatever i'll call you dissapointment ")
+if Don != "1" or "2" or "3" :
     print("Lunar:If you don't give me a serious answer i'll kill you and spare us both some time.")
-print("Lunar:If it's something ridiculous like steve, greg or Bob I will leave you here. ")
-input (">")
-input(name)
-print(f"Lunar:so your name is {name}")
-print("Lunar:So {name} let's get started and try not to die")
+print("Lunar:If it's something ridiculous like steve, greg or Bob I will leave you here. ",end = ' ')
+name = input()
+
+print(f"Lunar: so your name is {name}")
+print(f"Lunar:So {name} let's get started and try not to die")
 print(f"{name}:Ok ")
+print(f"{name}: What exactly are we doing")
+print("Lunar:That's for me to know and you to deal with for now lets just continue walking through these woods")
+print("""(Ten minutes have gone by as you walk in the dark and when you look around there is no sign of lunar what do you do)
+1.Shout for her agitated \n 2.Continue walking \n 3 Wait to see if she fell behind""")
+ml = input(">")
+if ml == "1":
+    print(f"{name}: Lunar Where the heck did u go this isn't funny")
+    print("Lunar: Aww did someone miss me a real warrior should be able to fend for themselves weakling im not here to take care of you")
+    print(f"{name}: What do you mean warrior I've been lost in the woods and you seem like the only way out except your little dissapearing act....where are you!.")
+    print("Lunar: I'm right on your shoulder")
+    print("(you look at your shoulder hesitantly to see a angry, small bright red gecko on it ")
+    color = "red"
+if ml == "2":
+    print(".........")
+    print(f"{name}: This silence dosen't feel right is... is something wrong")
+    print("Lunar: I don't know are't you forgetting something")
+    print(f"{name}: Wait! Where were you and, where are you I can't see you anywhere")
+    print("Lunar: I'm right on your shoulder")
+    print("(you look at your shoulder hesitantly to see a bright blue gecko on it ")
+    color = "blue"
+if ml =="3":
+    print(f"{name}: She still hasn't shown up that's strange she's the one who said to keep going.")
+    print("Lunar: That is true so why did you stop!")
+    print(f"{name}: Where have you been and where are you.")
+    print("Lunar: I'm right on your shoulder")
+    print("(you look at your shoulder hesitantly to see a bright green gecko on it. ")
+    color = "green"
+
+print("1.use Sarcasim \n 2.Show concern \n 3.Discomfort \n 4.Instict ")
+Di = input(">")
+if Di == "1" :
+    print("Wow! Gecko goddess cool alot better than before it suits you")
+if Di == "2" :
+    print("What happened to you are you okay")
+if Di == "3" :
+    print("Eww get off my shoulder \n (you try to smake the creature off your shoulder but the gecko continues to climb your body to avoid the attacks leading to an even more unpleasant experience.) ")
+if Di == "4" :
+    print("(You stare at the gecko on your shoulder for a minute silent. With all of your might you grab it off your shoulder and throw it into a shrub)")
+    print("The gecko comes back and with unnatural strength knocks you off balance and drags you into the same bush.\n (Words of advice don't throw animals)")
+
+print("(you continue walking throgh the woods and you come across a large white building revealed by the rising sun)")
+print(f"{name} : Hey Lunar is this the place \n Lunar : Yep this it \n {name} : cool im gonna start running hold on. \n Lunar: Wait What.")
+  
